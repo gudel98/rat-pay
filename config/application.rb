5c12a64759866bde272efe62a81293617c1b7f73
Egor Shpalov <egorshpalov1998@gmail.com>
2025-09-18 13:18:39 +0200

    [Github CI] Update locale, add translate for pay_button

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+I18n.default_locale = :en
+
 module RatPayApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+green="\033[0;32m"
+red="\033[0;31m"
+nc="\033[0m"
+
+run_command() {
+  echo -e "${green}========== $1 ==========${nc}"
+  if ! eval "$2"; then
+    echo -e "${red}❌ $1 failed!${nc}"
+    exit 1
+  else
+    echo -e " "
+  fi
+}
+
+run_command "Brakeman" "bundle exec brakeman"
+run_command "Rubocop" "bundle exec rubocop"
+run_command "RSpec" "bundle exec rspec"
+run_command "Cucumber" "bundle exec cucumber"
